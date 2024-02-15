a11c27c0f7dd0917470eea81d9a07ccf85e74bc4
José Danúbio <danubio.dm@hotmail.com>
2024-02-15 11:19:17 -0300

    ajustes

--- lib/pages/tela_sugestoes.dart
+++ lib/pages/tela_sugestoes.dart
@@ -2,6 +2,7 @@ import 'package:calculadora_bancario/components/custom_calc_button.dart';
 import 'package:calculadora_bancario/components/insert_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -33,16 +34,57 @@ class _TelaSugestaoState extends State<TelaSugestao> {
     final form = _formKey.currentState;
     if (form!.validate()) {
       form.save();
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(
+                  child: LoadingAnimationWidget.staggeredDotsWave(
+                    color: const Color.fromARGB(255, 0, 96, 164),
+                    size: 60,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "Enviando sugestão...",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 96, 164),
+                    ),
+                  ), // O texto
+                ),
+              ],
+            ),
+          );
+        },
+      );
       CollectionReference sugestoes =
           FirebaseFirestore.instance.collection('sugestoes');
-      await sugestoes.add({
-        'nome': nome,
-        'telefone': telefone,
-        'email': email,
-        'sugestao': sugestao,
-      });
-      Navigator.pop(context);
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      try {
+        await sugestoes.add({
+          'nome': nome,
+          'telefone': telefone,
+          'email': email,
+          'sugestao': sugestao,
+        });
+        Future.delayed(const Duration(milliseconds: 500), () {
+          Navigator.pop(context);
+          Navigator.pop(context);
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        });
+      } catch (e) {
+        Navigator.pop(context);
+        const snackBarErro = SnackBar(
+            content: Text(
+                'Erro ao enviar sugestão. Verifique sua conexão com a internet.'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBarErro);
+      }
     }
   }
 
